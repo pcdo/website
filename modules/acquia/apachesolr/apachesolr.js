c065847fdb3ba2e8d5adae04dc5fa9259b395099
buildbot <buildbot@08d20552-8ba5-4116-8c9e-e96c1c1f3480>
2009-07-30 18:47:12 +0000

    Acquia Drupal 1.2.14 (Drupal 6.13 core) has just been released and    contains important security updates and bug fixes. We recommend that    you update to the new version as soon as possible.     1. Please reference the [1]Getting Started Guide for information about        updating your site.     2. Next, [2]download the new version.
       Acquia Drupal consists of Acquia's package of Drupal core with a
       curated set of contributed modules, plus the ability to connect to the
       Acquia Network. Acquia Drupal 1.2.14 (Drupal 6.13 core) includes the
       following updates: ===================================================
         * Updated the Calendar module to [3]6.x-2.2. This also fixes a
           cross-site scripting security issue described in
           [4]SA-CONTRIB-2009-047.
         * Updated the Date module to [5]6.x-2.3. This also fixes a cross-site
           scripting security issue described in [6]SA-CONTRIB-2009-046. Note:
           When updating from extremely old versions of the Date module (Date
           6.x-2.0-rc6/Acquia Drupal 1.2.4 and earlier), errors can occur
           which will prevent future updates from running. If you are updating
           from one of these versions, you can fix the problem by either first
           updating to an intermediate version of the module, or by applying
           the patch found at http://drupal.org/node/535270 to your
           installation.
         * Updated the [7]Acquia Search package to 6.x-1.5071, which includes
           an update of the Apache Solr Search Integration module to
           [8]6.x-1.0-rc2.
         * Updated the FileField module to [9]6.x-3.1.
         * Updated the Image module to [10]6.x-1.0-alpha5.
         * Updated the ImageField module to [11]6.x-3.1.
         * Updated the Link module to [12]6.x-2.6.
         * Updated the Voting API module to [13]6.x-2.2.
    
       Notes:
         * Acquia Drupal may include released, beta, alpha, or patched modules
           depending on which version is determined to be the most current,
           stable, secure and appropriate version for general use.
         * Customers updating a production site should always perform a backup
           first, and, if possible, perform a trial update in a test
           environment before updating their production site.
         * For some of the modules being upgraded, it is important to make
           sure that the new version of the module completely replaces the old
           one (i.e., that the entire module's directory is replaced and that
           there are no files from the old version left behind). Please follow
           the recommended upgrade procedures found in the [14]Getting Started
           Guide.
         * If you experience problems during the upgrade, clearing Drupal's
           caches may be necessary. You can do this by visiting Administer >
           Site configuration > Performance (?q=admin/settings/performance)
           and clicking the "Clear cached data" button at the bottom of the
           page. Also note that you may be prompted to run your site's
           database updates more than once, and if you see error messages
           during the upgrade they do not necessarily indicate a problem.
    
    References
    
       1. http://acquia.com/documentation/getting-started/introduction
       2. http://acquia.com/downloads
       3. http://drupal.org/node/534336
       4. http://drupal.org/node/534652
       5. http://drupal.org/node/534332
       6. http://drupal.org/node/534636
       7. http://acquia.com/products-services/acquia-search
       8. http://drupal.org/node/535702
       9. http://drupal.org/node/509792
      10. http://drupal.org/node/516630
      11. http://drupal.org/node/509788
      12. http://drupal.org/node/512554
      13. http://drupal.org/node/525686
      14. http://acquia.com/documentation/getting-started/introduction
    
    
    git-svn-id: file:///mnt/www/svn//drupal/branches/1.x-6.x@5081 08d20552-8ba5-4116-8c9e-e96c1c1f3480

--- modules/acquia/apachesolr/apachesolr.js
+++ modules/acquia/apachesolr/apachesolr.js
@@ -1,9 +1,8 @@
-$(document).ready(function() {
+// $Id: apachesolr.js,v 1.1.2.4 2009/07/23 14:12:53 robertDouglass Exp $
 
+Drupal.behaviors.apachesolr = function(context) {
   $('.apachesolr-hidden-facet').hide();
-  
   $('<a href="#" class="apachesolr-showhide"></a>').text(Drupal.t('Show more')).click(function() {
-
     if ($(this).prev().find('.apachesolr-hidden-facet:visible').length == 0) {
       $(this).prev().find('.apachesolr-hidden-facet').show();
       $(this).text(Drupal.t('Show fewer'));
@@ -14,5 +13,4 @@ $(document).ready(function() {
     }
     return false;
   }).appendTo($('.block-apachesolr_search:has(.apachesolr-hidden-facet), .block-apachesolr:has(.apachesolr-hidden-facet)'));
-
-});
+}
